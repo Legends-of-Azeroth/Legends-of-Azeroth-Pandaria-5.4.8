88e15a4a006e90268ecc1650592859c7e3406a38
lee <leelf_00@126.com>
2026-07-26 11:35:33 +0800

    temp remove drop table

--- sql/updates/world/2025_03_02_00_world.sql
+++ sql/updates/world/2025_03_02_00_world.sql
@@ -42,4 +42,4 @@ INSERT IGNORE INTO `keychain`(`ID`, `Key1`, `Key2`, `Key3`, `Key4`, `Key5`, `Key
 SELECT `keyId`, `k1`, `k2`, `k3`, `k4`, `k5`, `k6`, `k7`, `k8`, `k9`, `k10`, `k11`, `k12`, `k13`, `k14`, `k15`, `k16`, `k17`, `k18`, `k19`, `k20`, `k21`, `k22`, `k23`, `k24`, `k25`, `k26`, `k27`, `k28`, `k29`, `k30`, `k31`, `k32`, 0
 FROM `keychain_db2`;
 
-DROP TABLE IF EXISTS `keychain_db2`;
+-- DROP TABLE IF EXISTS `keychain_db2`;
