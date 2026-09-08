5b6e9b9f4a8813ce5646221f0f640ddc9328cd4a
lee <leelf_00@126.com>
2026-09-08 22:35:03 +0800

    step 2

--- src/server/game/AI/CoreAI/GuardAI.cpp
+++ src/server/game/AI/CoreAI/GuardAI.cpp
@@ -61,7 +61,7 @@ void GuardAI::EnterEvadeMode(EvadeReason /*why*/)
         return;
     }
 
-    TC_LOG_TRACE("scripts.ai", "GuardAI::EnterEvadeMode: {} enters evade mode.", me->GetGUID().ToString());
+    TC_LOG_TRACE("scripts.ai", "GuardAI::EnterEvadeMode: {} enters evade mode.", me->GetGUID().ToString().c_str());
 
     me->RemoveAllAuras();
     me->CombatStop(true);
