5ffdef6f2107953b3a680f37ddd06b5d312a1080
lee <leelf_00@126.com>
2026-09-11 08:33:32 +0800

    step 2

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -16768,8 +16768,8 @@ void Player::RewardQuest(Quest const* quest, uint32 reward, Object* questGiver, 
         SendQuestReward(quest, XP);
 
     // cast spells after mark quest complete (some spells have quest completed state requirements in spell_area data)
-    if (quest->GetRewSpell() > 0)
-        CastSpell(this, quest->GetRewSpell(), true);
+    if (quest->GetRewDisplaySpell() > 0)
+        CastSpell(this, quest->GetRewDisplaySpell(), true);
     else if (quest->GetRewSpell() > 0)
         CastSpell(this, quest->GetRewSpell(), true);
 
